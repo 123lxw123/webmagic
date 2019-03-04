9a87d1ef20c09955539e18d27c92cb3e77ec5d2d
jike <jike@kezaihui.com>
2019-03-04 18:54:53 +0800

    update pom version

--- webmagic-core/src/test/java/us/codecraft/webmagic/proxy/SimpleProxyProviderTest.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/proxy/SimpleProxyProviderTest.java
@@ -20,11 +20,11 @@ public class SimpleProxyProviderTest {
         Proxy originProxy1 = new Proxy("127.0.0.1", 1087);
         Proxy originProxy2 = new Proxy("127.0.0.1", 1088);
         SimpleProxyProvider proxyProvider = SimpleProxyProvider.from(originProxy1, originProxy2);
-        Proxy proxy = proxyProvider.getProxy(TASK);
+        Proxy proxy = proxyProvider.getProxy(null, TASK);
         assertThat(proxy).isEqualTo(originProxy1);
-        proxy = proxyProvider.getProxy(TASK);
+        proxy = proxyProvider.getProxy(null, TASK);
         assertThat(proxy).isEqualTo(originProxy2);
-        proxy = proxyProvider.getProxy(TASK);
+        proxy = proxyProvider.getProxy(null, TASK);
         assertThat(proxy).isEqualTo(originProxy1);
     }
 }
